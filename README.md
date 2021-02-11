22278d24a8d821d0c6dcf3f15ccf66907906a948
crxty <crxty@users.noreply.github.com>
2021-02-11 21:05:50 +0100

    v.2
    More info in README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # COVID-Crna-Gora
- Tracker web app
+ This applicatoin was made to track the status of infection/recovery rates in Montenegro using simple calculation alghorytms using JavaScript.
+ The idea behind this is to try to predict how many days are left till complete infection/recovery/extinction.
+ Data is being collected from user input and later used in calculations, for now.
+# TODO
+-Parser for IJZ website
+-Cool animations
+-Responsive desing
+-Adaptive calculations based on postivie/negative ratio
+# Versions
+V.1 //23.11.2020//
+-Inital version 
+V.1.1 //21.01.2021//
+-Font -> IBM Plex Sans
+-Responsive design
+V.2 //11.02.2021//
+-Adaptive calculations
+-User input
+
--- script.js
+++ script.js
@@ -1,39 +1,60 @@
-//for 21.11.2020
-let newInfectToday = 574;
-let newRecovToday = 222;
-let newDeadToday = 5;
-const population = 622359;  //2018 
-let allInfectNow = 11090;
-let IRatio; 
-let DRatio;
-//calculating the day when everyone is infected CETERIS PARIBUS the infection growth is positive
-calcDayI = function() { 
-    this.allNewInfectToday = newInfectToday - newRecovToday - newDeadToday;
-    IRatio = (population - allInfectNow) / this.allNewInfectToday
-    // float to whole number transformation
-    wholeI = Math.trunc(IRatio);
-    return wholeI;
-}
-
-//calculating the day when everyone are dead CETERIS PARIBUS the infection and death growth is positive
-
-calcDayD = function () {
-    DRatio = population / newDeadToday;
-    wholeD = Math.trunc(DRatio);
-    return wholeD; 
-}
-
-
-document.getElementById('newInfected').innerHTML = newInfectToday;
-document.getElementById('newRecovered').innerHTML = newRecovToday;
-document.getElementById('newDead').innerHTML = newDeadToday;
-document.getElementById('estInfected').innerHTML = calcDayI(IRatio);
-document.getElementById('estDead').innerHTML = calcDayD(DRatio);
-
-//today's date block
+const population = 622359;  //2018
+let IRatio, DRatio, RRatio;
+let allNewInfectToday, allNewRecovToday;
+let newInfectToday, newRecovToday, newDeadToday, allInfectNow;
 let today = new Date();
-
 let date = today.getDate()+'   '+(today.getMonth()+1)+'   '+today.getFullYear();
 document.getElementById('date').innerHTML = date;
+
+tendency = () => {
+    if (newInfectToday > newRecovToday) {
+        calcDayD();
+        calcDayI();
+    } else {
+        calcDayR();    
+    }
+};
+
+// getting user input for calcualtions
+const button = document.getElementById('trigger');
+button.addEventListener('click', getInput);
+function getInput() {
+    newInfectToday = document.getElementById('newInfectedInput').value;
+    newRecovToday = document.getElementById('newRecoveredInput').value;
+    newDeadToday = document.getElementById('newDeadInput').value;
+    allInfectNow = document.getElementById('allInfectInput').value;
+    console.log(newInfectToday + ' new infected today.');
+    console.log(newRecovToday + ' new recovered today.');
+    console.log(newDeadToday + ' new dead today.');
+    console.log(allInfectNow + ' total infected now.');
+    tendency();
+}
+
+//calculating the day when everyone is infected CETERIS PARIBUS the infection growth is positive
+calcDayI = () => {
+    document.getElementById('bad').style.display = 'block';
+    allNewInfectToday = newInfectToday - newRecovToday - newDeadToday;
+    IRatio = (population - allInfectNow) / allNewInfectToday
+    // float to whole number transformation
+    wholeI = Math.trunc(IRatio);
+    document.getElementById('estInfected').innerHTML = wholeI;
+};
+
+//calculating the day when everyone are dead CETERIS PARIBUS the infection and death growth is positive
+calcDayD = () => {
+    document.getElementById('bad').style.display = 'block';
+    DRatio = population / newDeadToday;
+    wholeD = Math.trunc(DRatio);
+    document.getElementById('estDead').innerHTML = wholeD;
+};
+
+// positive growth tendency
+calcDayR = () =>  {
+    document.getElementById('good').style.display = 'block';
+    allNewRecovToday = newRecovToday - newInfectToday - newDeadToday;
+    RRatio = allInfectNow - allNewRecovToday;
+    wholeR = Math.trunc(RRatio);
+    document.getElementById('estRecov').innerHTML = wholeR;
+};
 
 
